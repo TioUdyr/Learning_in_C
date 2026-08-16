01eae81bfb92ce8fb07e1d612f6efe5d2d23b136
Udyr-dev <alex.minecraft121@gmail.com>
2026-08-16 00:09:34 -0300

    Soma de dois numeros

---
+++ Somando.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    int n1, n2, soma;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+
+    soma = n1 + n2;
+
+    printf("A soma dos dois numeros da: %d", soma);
+
+    return 0;
+}
