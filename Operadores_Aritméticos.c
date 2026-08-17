50cea94d3270cc0db45dab2a740214ba3002b0cc
TioUdyr <alex.minecraft121@gmail.com>
2026-08-16 21:12:44 -0300

    Fiz os operadores basicos aritméticos
    Adicionei soma, sub, mult, div, módulo.
    E na divisão fiz um if/else com operador ternário para não pular processos

---
+++ Operadores_Aritméticos.c
@@ -0,0 +1,96 @@
+/*
+#include <stdio.h>
+int main(){
+
+    int n1, n2, soma;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+
+    soma = n1 + n2;
+
+    printf("A soma dos dois numeros da : %d", soma);
+
+    return 0;
+}*/
+
+
+/*
+#include <stdio.h>
+int main(){
+
+    int n1, n2, sub;
+
+    printf("O segundo numero que vai subtrair o primeiro.\n");
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+
+    sub = n1 - n2;
+
+    printf("A subtracao dos dois numeros da : %d", sub);
+
+    return 0;
+}*/
+
+
+/*
+#include <stdio.h>
+int main(){
+
+    int n1, n2, mult;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+
+    mult = n1 * n2;
+
+    printf("A multiplicacao dos dois numeros da : %d", mult);
+
+
+    return 0;
+}*/
+
+
+/*
+#include <stdio.h>
+int main(){
+
+    float n1, n2, div;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%f", &n2);
+
+    div = (n2 != 0) ? (float)n1 / n2 :0;  // esse ? no meio da linha é o Operador ternário como se fosse um if. So que em uma linha só de código
+
+    printf("A divisao dos dois numeros da : %.2f", div);
+
+
+    return 0;
+}*/
+
+
+/*
+#include <stdio.h>
+int main(){
+    int n1;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+
+    printf("Se der 0 = par se for der 1 = impar\nResultado: %d", n1 % 2);
+
+    return 0;
+}*/
