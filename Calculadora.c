23511805c472258243b91906fb7f23df7400d02a
Udyr-dev <alex.minecraft121@gmail.com>
2026-08-17 19:09:02 -0300

    fazendo uma calculadora
    esqueleto

---
+++ Calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    double n1, n2, operador;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%lf", &n1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%lf", &n2);
+
+    printf("Digite o operador (+, -, *, /)");
+    scanf("%lf", &operador);
+    
+    if (operador == '+')
+
+    return 0;
+}
