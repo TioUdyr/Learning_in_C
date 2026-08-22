6b8823928079edd5e77e50b380260b352dbd6f14
Udyr-dev <alex.minecraft121@gmail.com>
2026-08-22 16:05:59 -0300

    Alguns desafios da Aula
    Controle de entrada de pessoas

---
+++ Controle_de_entrada.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main (){
+
+    int idade, ingresso;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Você tem ingresso?");
+    scanf("%d", &ingresso);
+
+    if(idade >= 18 && ingresso == 'sim'){
+        printf("Voce pode entrar");
+    }
+    else{
+        printf("Voce nao pode entrar");
+    }
+
+
+
+
+    return 0;
+}
+
+//precisa ter 18 anos
+// ter ingresso ou nao
+//obrigatorio atender as duas requisiçoes caso contrario nao entra
