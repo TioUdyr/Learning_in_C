060c28ca2ba0b42c42a1aef90964782729420341
TioUdyr <alex.minecraft121@gmail.com>
2026-08-26 06:45:53 -0300

    Alterações para o CRUD
    usando fgets para nao dar problema nos numeros

--- CRUD.c
+++ CRUD.c
@@ -10,6 +10,7 @@ struct pessoas{
 int main(){
     struct pessoas p [5];
     int i;
+    char buffer[50]; // Esse cara ajuda a trocar 
 // Pedindo os dados da pessoa.
 
     
@@ -19,12 +20,13 @@ int main(){
         p[i].nome[strcspn(p[i].nome, "\n")] = '\0';
     
         printf("Digite a idade da pessoa: ");
-        scanf("%d", &p[i].idade);
-        getchar();
+        fgets(buffer, sizeof(buffer), stdin);
+        p[i].idade = atoi(buffer); // O atoi ajuda a converter a string pra 0 para nao dar erro.
     
-        printf("Digite o numero da pessoa: \n");
-        scanf("%d", &p[i].telefone);
-        getchar();}
+        printf("Digite o numero da pessoa: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        p[i].telefone = atoi(buffer);
+    }
 // ler a lista de Pessoas
     printf("\n----- Lista de Usuarios-----\n");
     for(i = 0 ; i < 5; i++){
